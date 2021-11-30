aab9823f37e1c28b54cb753c0aeffdcccf75f75a
Shawlycode <kutinaorlando@gmail.com>
2021-11-30 11:21:04 +0000

    created various screens

---
+++ src/components/Screens/GetStartedScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const GetStartedScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View></View>
+      <View></View>
+      <View></View>
+    </View>
+  );
+};
+
+export default GetStartedScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
