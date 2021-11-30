8b8dd586e3c37e58279de77cec4dfaf0e6d2abfe
Shawlycode <kutinaorlando@gmail.com>
2021-11-30 11:20:38 +0000

    created welcomeScreen

---
+++ src/components/Screens/WelcomeScreen.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const WelcomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <Image
+          source={require("../../../assets/ampersand_logo.png")}
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={styles.title}>
+        <Text style={styles.titleText}>ampersand</Text>
+        <Text style={styles.titleText}>contacts</Text>
+      </View>
+      <View style={styles.getStartedBtn}>
+        <TouchableOpacity>
+          <Text style={styles.getStartedBtnText}>Get started</Text>
+          <View style={styles.btn}></View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+  },
+  logo: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoImage: {
+    resizeMode: "contain",
+    width: 250,
+    height: 250,
+    justifyContent: "center",
+  },
+  title: {
+    flex: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleText: {
+    fontSize: 30,
+    textTransform: "uppercase",
+    color: "#8A878C",
+  },
+  getStartedBtn: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  getStartedBtnText: {
+    fontSize: 20,
+    textTransform: "uppercase",
+    color: "#8A878C",
+  },
+  btn: {
+    width: 120,
+    height: 10,
+    borderBottomWidth: 4,
+    borderBottomColor: "red",
+  },
+});
