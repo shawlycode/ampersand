40a9bd1b2c0fed656a430e2109c112d58365dd6a
Shawlycode <kutinaorlando@gmail.com>
2021-12-04 09:52:42 +0000

    rendered route

--- App.js
+++ App.js
@@ -1,24 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import GetStartedScreen from "./src/components/Screens/GetStartedScreen";
-// import { StyleSheet, Text, View } from "react-native";
-import WelcomeScreen from "./src/components/Screens/WelcomeScreen";
+import Route from "./Route/Route";
 
 export default function App() {
-  return (
-    // <View style={styles.container}>
-    <GetStartedScreen />
-
-    // <StatusBar style="auto" />
-    // </View>
-  );
+  return <Route />;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
